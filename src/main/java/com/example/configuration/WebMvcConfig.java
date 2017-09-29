5dcedb8be8590ed6b563500afb08208741e651ff
Minh Than <thanqminh@gmail.com>
2017-09-29 15:36:20 +0700

    fix view unicode

--- src/main/java/com/example/configuration/WebMvcConfig.java
+++ src/main/java/com/example/configuration/WebMvcConfig.java
@@ -1,9 +1,14 @@
 package com.example.configuration;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
@@ -13,5 +18,28 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 		return bCryptPasswordEncoder;
 	}
+	
+	@Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setContentType("text/html; charset=UTF-8");    		
+        return resolver;
+    }
+	
+	@Bean
+	public FilterRegistrationBean filterRegistrationBean() {
+	    FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+	    CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+	    characterEncodingFilter.setForceEncoding(true);
+	    characterEncodingFilter.setEncoding("UTF-8");
+	    registrationBean.setFilter(characterEncodingFilter);
+	    return registrationBean;
+	}
+
+    @Override
+    public void configureDefaultServletHandling(
+        DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
 
 }
